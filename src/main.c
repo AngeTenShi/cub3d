16cac0d6bc78c6bef60d722a0ce76753cbc8b432
AngeTenShi <megazombie06pro@gmail.com>
2022-10-19 12:40:50 +0200

    initializing parsing

--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
+#include "../cub3d.h"
 
-#include "cub3d.h"
+void	display_error(char *type)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(type, 2);
+}
 
 int	main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		printf("Error\nExec with format : ./cub3d map.cub\n");
+		display_error("Exec with format : ./cub3d map.cub\n");
 		return (0);
 	}
+	else
+		parse_args(ac, av);
+	//init_map();
+	return (1);
 }
