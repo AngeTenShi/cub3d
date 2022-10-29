d0d5bee1d7c5abd4093dc3fb7ebbe3085ad0e294
AngeTenShi <megazombie06pro@gmail.com>
2022-10-29 16:17:22 +0200

    creating structure

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anggonza <anggonza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 16:14:27 by anggonza          #+#    #+#             */
+/*   Updated: 2022/10/29 16:14:31 by anggonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../cub3d.h"
 
@@ -15,9 +26,20 @@ int	check_format(char *map_path)
 	return (1);
 }
 
-int	parse_args(int ac, char **av)
+int	parse_args(char **av)
 {
-	if (!check_format(av[1]))
-		return (0);
+	int	fd;
+	char	buffer[65535];
+	char	**map;
+
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		display_error(""); 
+	read(fd, buffer, 65535);
+	if (read < 0 || !check_format(av[1]))
+		display_error("Bad file format .cub");
+	map = ft_split(buffer, "\n");
+	
+
 	return (1);
 }
